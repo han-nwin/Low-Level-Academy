0f40bc9c1f04befe7713220b3aff2072ddbddf97
han-nwin <hannguyen.win@gmail.com>
2024-10-29 23:15:41 -0500

    d

--- C-Zero2Hero/struct-union.c
+++ C-Zero2Hero/struct-union.c
@@ -24,7 +24,19 @@ int main () {
   union Data data;
 
   data.intValue = 10;
-  
+    printf("Int Value: %d\n", data.intValue);
+
+    data.floatValue = 3.14;
+    printf("Float Value: %.2f\n", data.floatValue);
+
+    data.charValue = 'A';
+    printf("Char Value: %c\n", data.charValue);
+
+    // Notice that only the last value assigned (charValue) is correctly printed
+    printf("\nAfter all assignments:\n");
+    printf("Int Value: %d\n", data.intValue);
+    printf("Float Value: %.2f\n", data.floatValue);
+    printf("Char Value: %c\n", data.charValue);
 
 
 
